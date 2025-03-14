555564a19f26503bb31db54e9115eb9c8bae1d5f
sheabali <iamskpranto@gmail.com>
2025-03-14 17:15:44 +0600

    Design Featured Products Section

--- src/app/(withCommonLayout)/(home)/page.tsx
+++ src/app/(withCommonLayout)/(home)/page.tsx
@@ -1,4 +1,5 @@
 import Category from '@/components/modules/home/Category';
+import FeaturedProducts from '@/components/modules/home/FeaturedProducts';
 import HeroSection from '@/components/modules/home/HeroSection';
 
 const HomePage = () => {
@@ -6,6 +7,7 @@ const HomePage = () => {
     <div>
       <HeroSection />
       <Category />
+      <FeaturedProducts />
     </div>
   );
 };
