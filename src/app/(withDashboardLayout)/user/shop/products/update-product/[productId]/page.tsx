d5086e72bd4ca6e1fbb1d04dc41d8508f4f2ac4d
sheabali <iamskpranto@gmail.com>
2025-03-14 13:57:12 +0600

    Update Product Info

---
+++ src/app/(withDashboardLayout)/user/shop/products/update-product/[productId]/page.tsx
@@ -0,0 +1,20 @@
+import UpdateProductForm from '@/components/modules/shop/product/UpdateProductForm';
+import { getSingleProduct } from '@/services/Product';
+
+const UploadProduct = async ({
+  params,
+}: {
+  params: Promise<{ productId: string }>;
+}) => {
+  const { productId } = await params;
+
+  const { data: product } = await getSingleProduct(productId);
+
+  return (
+    <div className="flex justify-center items-center">
+      <UpdateProductForm product={product} />
+    </div>
+  );
+};
+
+export default UploadProduct;
