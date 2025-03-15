3374664388c4d6b0ab42b1082ebe305507be933e
sheabali <iamskpranto@gmail.com>
2025-03-15 16:15:25 +0600

    Create All Products Banner & Category Section

--- src/app/(withCommonLayout)/products/page.tsx
+++ src/app/(withCommonLayout)/products/page.tsx
@@ -1,5 +1,23 @@
-const AllProductsPage = () => {
-  return <div>All Products Page</div>;
+import AllProducts from '@/components/modules/products';
+import ProductBanner from '@/components/modules/products/banner';
+import CategoryCard from '@/components/ui/core/CategoryCard';
+import NMContainer from '@/components/ui/core/NMContainer';
+import { getAllCategory } from '@/services/Category';
+import { ICategory } from '@/types';
+
+const AllProductsPage = async () => {
+  const { data: categories } = await getAllCategory();
+  return (
+    <NMContainer>
+      <ProductBanner title="All Products" path="Home - Product" />;
+      <div className="grid grid-cols-6 gap-8 my-5">
+        {categories.map((category: ICategory, idx: number) => (
+          <CategoryCard key={idx} category={category} />
+        ))}
+      </div>
+      <AllProducts />
+    </NMContainer>
+  );
 };
 
 export default AllProductsPage;
---
+++ src/components/modules/products/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AllProducts = () => {
+  return (
+    <div className="flex">
+      <div>sidebar</div>
+      <div>products</div>
+    </div>
+  );
+};
+
+export default AllProducts;
