deae030ad69c6fccb71b27bb41ebee7bee3d72e8
sheabali <iamskpranto@gmail.com>
2025-03-15 23:48:37 +0600

    Create Product Cart Page

--- src/app/(withCommonLayout)/cart/page.tsx
+++ src/app/(withCommonLayout)/cart/page.tsx
@@ -1,3 +1,6 @@
+import CartProductCard from '@/components/modules/cart/CartProductCard';
+import CartProducts from '@/components/modules/cart/CartProducts';
+import Coupon from '@/components/modules/cart/Coupon';
 import ProductBanner from '@/components/modules/products/banner';
 import NMContainer from '@/components/ui/core/NMContainer';
 
@@ -5,6 +8,10 @@ const CartPage = () => {
   return (
     <NMContainer>
       <ProductBanner title="Cart Page" path="Home - Product - Cart Page" />
+      <div className="grid grid-cols-12 gap-8 my-5">
+        <CartProducts />
+        <Coupon />
+      </div>
     </NMContainer>
   );
 };
