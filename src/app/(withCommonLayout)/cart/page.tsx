56dbaf068fc1ad090d4ff423fa5a5df037ddfa76
sheabali <iamskpranto@gmail.com>
2025-03-15 23:41:18 +0600

    set cart routes

---
+++ src/app/(withCommonLayout)/cart/page.tsx
@@ -0,0 +1,12 @@
+import ProductBanner from '@/components/modules/products/banner';
+import NMContainer from '@/components/ui/core/NMContainer';
+
+const CartPage = () => {
+  return (
+    <NMContainer>
+      <ProductBanner title="Cart Page" path="Home - Product - Cart Page" />
+    </NMContainer>
+  );
+};
+
+export default CartPage;
