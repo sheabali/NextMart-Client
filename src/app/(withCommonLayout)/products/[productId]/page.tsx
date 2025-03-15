f11111f6d24f6792c844c06a7fe0bae133d33028
sheabali <iamskpranto@gmail.com>
2025-03-15 23:19:12 +0600

    Create Product Cart Page

--- src/app/(withCommonLayout)/products/[productId]/page.tsx
+++ src/app/(withCommonLayout)/products/[productId]/page.tsx
@@ -1,12 +1,26 @@
+import ProductBanner from '@/components/modules/products/banner';
+import ProductDetails from '@/components/modules/products/productDetails';
+import NMContainer from '@/components/ui/core/NMContainer';
+import { getAllProducts, getSingleProduct } from '@/services/Product';
 import React from 'react';
 
 const ProductDetailsPage = async ({
   params,
 }: {
-  params: { productId: string };
+  params: Promise<{ productId: string }>;
 }) => {
-  console.log(params);
-  return <div>{params.productId}</div>;
+  const { productId } = await params;
+  const { data: product } = await getSingleProduct(productId);
+
+  return (
+    <NMContainer>
+      <ProductBanner
+        title="Product - Details"
+        path="Home - Product - Product Details"
+      />
+      <ProductDetails product={product} />
+    </NMContainer>
+  );
 };
 
 export default ProductDetailsPage;
