ba86a1d92996c1c81ea25786a54da553fc4cc218
sheabali <iamskpranto@gmail.com>
2025-03-16 23:16:14 +0600

    redux state connented

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -2,9 +2,14 @@
 
 import UserProvider from '@/context/UserContext';
 import React from 'react';
+import StoreProvider from './StoreProvider';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <UserProvider>{children}</UserProvider>;
+  return (
+    <UserProvider>
+      <StoreProvider>{children}</StoreProvider>
+    </UserProvider>
+  );
 };
 
 export default Providers;
