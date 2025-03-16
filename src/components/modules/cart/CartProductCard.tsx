ac57310b56a5643d9a548ddf4c2343f1341c1493
sheabali <iamskpranto@gmail.com>
2025-03-17 01:06:03 +0600

    Updating Quantity and Removing Items

--- src/components/modules/cart/CartProductCard.tsx
+++ src/components/modules/cart/CartProductCard.tsx
@@ -1,10 +1,27 @@
 import { Button } from '@/components/ui/button';
-import { CartProduct } from '@/redux/features/cartSlice';
-import { IProduct } from '@/types';
+import {
+  CartProduct,
+  decrementOrderQuantity,
+  incrementOrderQuantity,
+  removeProduct,
+} from '@/redux/features/cartSlice';
+import { useAppDispatch } from '@/redux/hooks';
 import { Minus, Plus, Trash } from 'lucide-react';
 import Image from 'next/image';
 
 export default function CartProductCard({ product }: { product: CartProduct }) {
+  const dispatch = useAppDispatch();
+
+  const handleIncrementProduct = (id: string) => {
+    dispatch(incrementOrderQuantity(id));
+  };
+  const handleDecrementProduct = (id: string) => {
+    dispatch(decrementOrderQuantity(id));
+  };
+  const handleRemoveProduct = (id: string) => {
+    dispatch(removeProduct(id));
+  };
+
   return (
     <div className="bg-white rounded-lg flex p-5 gap-5">
       <div className="h-full w-32 rounded-md overflow-hidden">
@@ -36,16 +53,28 @@ export default function CartProductCard({ product }: { product: CartProduct }) {
           </h2>
           <div className="flex items-center gap-2">
             <p className="text-gray-500 font-semibold">Quantity</p>
-            <Button variant="outline" className="size-8 rounded-sm">
+            <Button
+              onClick={() => handleDecrementProduct(product?._id)}
+              variant="outline"
+              className="size-8 rounded-sm"
+            >
               <Minus />
             </Button>
             <p className="font-semibold text-xl p-2">
               {product?.orderQuantity}
             </p>
-            <Button variant="outline" className="size-8 rounded-sm">
+            <Button
+              onClick={() => handleIncrementProduct(product?._id)}
+              variant="outline"
+              className="size-8 rounded-sm"
+            >
               <Plus />
             </Button>
-            <Button variant="outline" className="size-8 rounded-sm">
+            <Button
+              onClick={() => handleRemoveProduct(product?._id)}
+              variant="outline"
+              className="size-8 rounded-sm"
+            >
               <Trash className="text-red-500/50" />
             </Button>
           </div>
--- src/redux/features/cartSlice.ts
+++ src/redux/features/cartSlice.ts
@@ -30,12 +30,41 @@ const cartSlice = createSlice({
 
       state.products.push({ ...action.payload, orderQuantity: 1 });
     },
+
+    incrementOrderQuantity: (state, action) => {
+      const productToIncrement = state.products.find(
+        (product) => product._id === action.payload
+      );
+      if (productToIncrement) {
+        productToIncrement.orderQuantity += 1;
+        return;
+      }
+    },
+    decrementOrderQuantity: (state, action) => {
+      const productToDecrement = state.products.find(
+        (product) => product._id === action.payload
+      );
+      if (productToDecrement && productToDecrement.orderQuantity > 1) {
+        productToDecrement.orderQuantity -= 1;
+        return;
+      }
+    },
+    removeProduct: (state, action) => {
+      state.products = state.products.filter(
+        (product) => product._id !== action.payload
+      );
+    },
   },
 });
 
 export const orderedProductsSelector = (state: RootState) => {
   return state.cart.products;
 };
-export const { addProduct } = cartSlice.actions;
+export const {
+  addProduct,
+  incrementOrderQuantity,
+  decrementOrderQuantity,
+  removeProduct,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
