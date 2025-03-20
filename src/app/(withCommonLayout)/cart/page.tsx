31f0a7ca89a2125939af1918c8c1e277ca2f264c
sheabali <iamskpranto@gmail.com>
2025-03-20 16:38:20 +0600

    fix error

--- src/app/(withCommonLayout)/cart/page.tsx
+++ src/app/(withCommonLayout)/cart/page.tsx
@@ -1,9 +1,9 @@
-import Address from "@/components/modules/cart/Address";
-import CartProducts from "@/components/modules/cart/CartProducts";
-import Coupon from "@/components/modules/cart/Coupon";
-import PaymentDetails from "@/components/modules/cart/PaymentDetails";
-import ProductBanner from "@/components/modules/products/banner";
-import NMContainer from "@/components/ui/core/NMContainer";
+import Address from '@/components/modules/cart/Address';
+import CartProducts from '@/components/modules/cart/CartProducts';
+import Coupon from '@/components/modules/cart/Coupon';
+import PaymentDetails from '@/components/modules/cart/PaymentDetails';
+import ProductBanner from '@/components/modules/products/banner';
+import NMContainer from '@/components/ui/core/NMContainer';
 
 const CartPage = () => {
   return (
